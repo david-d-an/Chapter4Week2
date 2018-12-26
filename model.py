2d8bf898685dea379a69e00db3cae9516757b066
David An <dong82@gmail.com>
2018-12-26 09:56:00 -0500

    happhouse model complete

--- model.py
+++ model.py
@@ -1,9 +1,29 @@
+# import h5py
 import numpy as np
-import h5py
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+# from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pylab
+
+from matplotlib.pyplot import imshow
+
 
 
 def mainfunc():
